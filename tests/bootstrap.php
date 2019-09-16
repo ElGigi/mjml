092e9963df00feb015f4d83563f3d5f221163de8
Ronan Giron <ElGigi@users.noreply.github.com>
2019-09-16 16:08:09 +0200

    Tests & copyright

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,17 +8,8 @@
  * file that was distributed with this source code, to the root.
  */
 
-declare(strict_types=1);
-
-namespace ElGigi\Mjml;
-
-use Exception;
-
-/**
- * Class MjmlException.
- *
- * @package ElGigi\MJML
- */
-class MjmlException extends Exception
-{
+$file = __DIR__ . '/../vendor/autoload.php';
+if (!file_exists($file)) {
+    throw new RuntimeException('Install dependencies using composer to run the test suite.');
 }
+$autoload = require_once $file;
